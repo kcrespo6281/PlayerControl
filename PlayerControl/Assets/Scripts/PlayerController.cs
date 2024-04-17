228c59aa28f17be3d1a51cec14623fd328de2518
katelyn <crespok@roverkids.org>
2024-04-17 13:05:37 -0400

    1.2 prj work stuff

--- PlayerControl/Assets/Scripts/PlayerController.cs
+++ PlayerControl/Assets/Scripts/PlayerController.cs
@@ -14,6 +14,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Move the vehicle foward
-        transform.Translate(Vector3.forward * Time.deltaTime;
+        transform.Translate(Vector3.forward * Time.deltaTime);
     }
 }
