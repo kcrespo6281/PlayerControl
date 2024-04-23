d50af72772e6a2cd84f8689eab31a96ba302b308
katelyn <crespok@roverkids.org>
2024-04-23 13:02:46 -0400

    directions?

--- PlayerControl/Assets/Scripts/PlayerController.cs
+++ PlayerControl/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizantalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +16,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizantalInput = Input.GetAxis("Horizantal");
         // Move the vehicle foward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizantalInput);
     }
+
+
 }
